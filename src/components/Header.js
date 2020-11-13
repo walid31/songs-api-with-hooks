1c8f5e3bead3f6c5211a5fd9ec4f0c2285f3d5d1
walid31 <ghalemwalid31@gmail.com>
2020-11-13 11:58:18 +0100

    created necessary component before staritng the funny work

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <nav id ="navifation">
+            <h1 href="#" className="logo"> mySongs</h1>
+        </nav>
+    )
+};
+
+export default Header;
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.svg';
+import {AuthContext} from "../App";
+
+export const Login = () => {
+    return (
+        <div className ="login-container">
+            <div className="card">
+                <div className="container">
+                    
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Login;
